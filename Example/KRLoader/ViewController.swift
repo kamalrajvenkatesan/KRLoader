579a07d50605348bd56a66e17e3bfa2826cc0f9c
Kamalraj Venkatesan <kamalraj.venkatesan@gyanmatrix.com>
2018-09-03 17:36:09 +0530

    Added ui objects for example

--- Example/KRLoader/ViewController.swift
+++ Example/KRLoader/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var loader: KRLoader!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -19,6 +21,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  @IBAction func startAnimating(_ sender: UIButton) {
 
+  }
+  @IBAction func stopAnimating(_ sender: UIButton) {
+    
+  }
+  
 }
 
